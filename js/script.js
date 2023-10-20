5fc7fc2963c54960d2b374fdab528d815353c13a
Alexandr Rusin <i@arusin.ru>
2023-10-20 15:40:55 +0300

    first-commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const btn = document.querySelector(".btn");
+const paranja = document.querySelector(".paranja");
+const formWrapper = document.querySelector(".form-wrapper");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("click!");
+  paranja.classList.toggle("open");
+  formWrapper.classList.toggle("open");
+  body.classList.toggle("block");
+});
+
+formWrapper.addEventListener("touchstart", handleTouchStart);
+formWrapper.addEventListener("touchmove", handleTouchMove);
+
+let x1 = null;
+let y1 = null;
+
+function handleTouchStart(event) {
+  const firstTouch = event.touches[0];
+  x1 = firstTouch.clientX;
+  y1 = firstTouch.clientY;
+}
+
+function handleTouchMove(event) {
+  if (!x1 || !y1) {
+    return false;
+  }
+
+  let x2 = event.touches[0].clientX;
+  let y2 = event.touches[0].clientY;
+
+  let xDif = x2 - x1;
+  let yDif = y2 - y1;
+
+  if (Math.abs(xDif) > Math.abs(yDif)) {
+    if (xDif > 0) console.log("right");
+    else console.log("left");
+  } else {
+    if (yDif > 0 && yDif > 50) {
+      paranja.classList.remove("open");
+      formWrapper.classList.remove("open");
+      body.classList.remove("block");
+      console.log("bottom");
+      console.log(yDif);
+    } else {
+      console.log("top");
+    }
+  }
+}
